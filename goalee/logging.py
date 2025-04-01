1999913a23e61aa4b46fad9bdb9f5dcd804afbd9
Konstantinos Panayiotou <klpanagi@gmail.com>
2025-04-01 17:42:44 +0300

    Configure logging via env variables

--- goalee/logging.py
+++ goalee/logging.py
@@ -2,11 +2,16 @@ import logging
 import os
 from rich.logging import RichHandler
 
-LOGGING_FORMAT = "%(message)s"
+ZERO_LOGS = int(os.getenv('ZERO_LOGS', 0))
 LOG_LEVEL = os.getenv("GOALDSL_LOG_LEVEL", "INFO")
 
-logging.basicConfig(
-    level=LOG_LEVEL, format=LOGGING_FORMAT, datefmt="[%X]", handlers=[RichHandler()]
-)
+if ZERO_LOGS:
+    logging.disable()
+else:
+    LOGGING_FORMAT = "%(message)s"
+    logging.basicConfig(
+        format=LOGGING_FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+    )
 
 default_logger = logging.getLogger()
+default_logger.setLevel(LOG_LEVEL)
